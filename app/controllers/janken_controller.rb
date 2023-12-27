8fb3388742d3bc725f289262bdeb4ff9cb3f03bb
Daisuke-Iwao <daisuke.iwao@luxiar.com>
2023-12-27 16:10:46 +0900

    じゃんけんできる様にした。

--- app/controllers/janken_controller.rb
+++ app/controllers/janken_controller.rb
@@ -4,6 +4,14 @@ class JankenController < ApplicationController
 
   def result
     @hand = params[:hand]
+    @computer_hand = ["グー", "チョキ", "パー"].sample
+    if @hand == @computer_hand
+      @result = "あいこです。"
+    elsif @hand == "グー" && @computer_hand == "チョキ" || @hand == "チョキ" && @computer_hand == "パー" || @hand == "パー" && @computer_hand == "グー"
+      @result = "あなたの勝ちです。"
+    else
+      @result = "あなたの負けです。"
+    end
   end
 
   def history
