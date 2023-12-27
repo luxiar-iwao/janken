20501820ec6c71d0980ec2de18aa040fb562e37e
Daisuke-Iwao <daisuke.iwao@luxiar.com>
2023-12-27 14:03:42 +0900

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'janken/select'
-  get 'janken/result'
-  get 'janken/history'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "janken#select"
+  get '/select', to: 'janken#select'
+  get '/result', to: 'janken#result'
+  get '/history', to: 'janken#history'
 end
